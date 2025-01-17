80cc4620fa36c9a0a57efb768b42db376e7e8842
StellaJiang58 <137820436+StellaJiang58@users.noreply.github.com>
2025-01-16 20:24:20 -0500

    Update sde_config.py

--- src/bitcoinmonitor/utils/sde_config.py
+++ src/bitcoinmonitor/utils/sde_config.py
@@ -2,21 +2,13 @@ import os
 from bitcoinmonitor.utils.db import DBConnection
 
 
-# def get_warehouse_creds() -> DBConnection:
-#     return DBConnection(
-#         user=os.getenv('WAREHOUSE_USER', ''),
-#         password=os.getenv('WAREHOUSE_PASSWORD', ''),
-#         db=os.getenv('WAREHOUSE_DB', ''),
-#         host=os.getenv('WAREHOUSE_HOST', ''),
-#         port=int(os.getenv('WAREHOUSE_PORT', 5432)),
-#     )
-    
 def get_warehouse_creds() -> DBConnection:
     return DBConnection(
-        user='postgres',
-        password='3006',
-        db='postgres',
-        host='127.0.0.1',
-        port='5432',
+        user=os.getenv('WAREHOUSE_USER', ''),
+        password=os.getenv('WAREHOUSE_PASSWORD', ''),
+        db=os.getenv('WAREHOUSE_DB', ''),
+        host=os.getenv('WAREHOUSE_HOST', ''),
+        port=int(os.getenv('WAREHOUSE_PORT', 5432)),
     )
+    
     
